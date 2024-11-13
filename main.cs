7a63e6f1ed8df82dc7131c8be0b50f0d389afb57
Raditya Alif <ditya818@gmail.com>
2024-11-13 13:52:58 +0700

    First Commit

---
+++ main.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace GameDev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Game Development!");
+            // Your game logic here
+        }
+    }
+}
